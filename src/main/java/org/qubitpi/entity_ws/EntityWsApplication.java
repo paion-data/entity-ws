7069beb81969e8ca3b3f4769b9064884d997a994
Jack <jack20220723@gmail.com>
2025-04-23 22:42:05 +0800

    Project init using fast-spring with 'curl -G http://localhost:8080/starter.zip -d dependencies=web -d javaVersion=17 -d name=entity-ws -d groupId=org.qubitpi -d artifactId=entity-ws -d packaging=jar -o entity-ws.zip'

---
+++ src/main/java/org/qubitpi/entity_ws/EntityWsApplication.java
@@ -0,0 +1,13 @@
+package org.qubitpi.entity_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/qubitpi/entity_ws/EntityWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qubitpi.entity_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
