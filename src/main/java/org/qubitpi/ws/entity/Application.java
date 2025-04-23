93380acfb038dac5b4ae99f765307e946f419c5d
Jack <jack20220723@gmail.com>
2025-04-23 22:49:16 +0800

    Rename 'EntityWsApplication' to 'Application'

--- src/main/java/org/qubitpi/ws/entity/Application.java
+++ src/main/java/org/qubitpi/ws/entity/Application.java
@@ -15,14 +15,14 @@
  */
 package org.qubitpi.ws.entity;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class EntityWsApplicationTests {
+@SpringBootApplication
+public class Application {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/org/qubitpi/ws/entity/ApplicationTests.java
+++ src/test/java/org/qubitpi/ws/entity/ApplicationTests.java
@@ -15,14 +15,14 @@
  */
 package org.qubitpi.ws.entity;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class EntityWsApplication {
+@SpringBootTest
+class ApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EntityWsApplication.class, args);
+	@Test
+	void contextLoads() {
 	}
 
 }
