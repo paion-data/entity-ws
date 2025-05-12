6f95b51b5d2fef0b6495908db2a27d0ebfe32d97
Jiaqi Liu <jack20220723@gmail.com>
2025-05-12 13:11:11 +0800

    Add documentation site (#13)

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,124 @@
+/**
+ * Copyright 2025 Jiaqi Liu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Entity Webservice',
+  tagline: 'Webservice with first-class support for Graph Database',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://entity-ws.qubitpi.org',
+  baseUrl: '/',
+
+  organizationName: 'QubitPi',
+  projectName: 'entity-ws',
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl:
+              'https://github.com/QubitPi/entity-ws/tree/master/docs',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Entity WS',
+      logo: {
+        alt: 'entity-ws Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: "https://entity-ws.qubitpi.org/apidocs",
+          label: "API",
+          position: "left",
+        },
+        {
+          href: 'https://github.com/QubitPi/entity-ws',
+          label: ' ',
+          position: 'right',
+          className: 'header-icon-link header-github-link',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Documentation',
+              to: '/docs/intro',
+            },
+            {
+              label: 'ArangoDB',
+              href: 'https://arango.qubitpi.org/stable/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/QubitPi/entity-ws',
+            },
+            {
+                label: 'Jiaqi Liu',
+                href: 'https://github.com/QubitPi',
+            },
+            {
+                label: "Jiaqi's Blog",
+                href: 'https://leadership.qubitpi.org/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Jiaqi Liu. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ["java", "bash", "json"]
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
